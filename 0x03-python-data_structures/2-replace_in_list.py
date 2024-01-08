3ec009325f5fd43be123a0f1631f0c1c167b2479
LL0YD7 <117781911+LL0YD7@users.noreply.github.com>
2024-01-08 18:03:15 +0200

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):  # Check if idx is negative or out of range
         return my_list  # Return the original list if idx is negative or out of range
