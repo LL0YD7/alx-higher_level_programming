5d50962537a6995508bb9056d12d751831142295
LL0YD7 <117781911+LL0YD7@users.noreply.github.com>
2024-01-08 18:02:28 +0200

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):  # Check if idx is negative or out of range
         return None  # Return None if idx is negative or out of range
