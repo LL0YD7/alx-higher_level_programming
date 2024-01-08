8df2a7d0fb894dd9017e345b138ee5f4d8e9fb59
LL0YD7 <117781911+LL0YD7@users.noreply.github.com>
2024-01-08 18:04:44 +0200

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):  # Check if idx is negative or out of range
-        return my_list[:]  # Return a copy of the original list using list slicing
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
     else:
-        new_list = my_list[:]  # Create a copy of the original list
-        new_list[idx] = element  # Replace the element at the specified index with the new element
-        return new_list  # Return the modified list
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
 
